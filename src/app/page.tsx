6e4d6d089fdfaf094282585d8a94bf0c588f880b
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2025-02-02 16:41:57 +0600

    developed: professioanl card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import TopProfessionaCard from "@/components/shared/cards/top-professional-card";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div className="w-full min-h-screen flex justify-center items-center">
+      <TopProfessionaCard />
+    </div>
+  );
 }
