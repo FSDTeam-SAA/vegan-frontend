b784ab6a7a6ff230f0acc5ddd136f0d59cf9207a
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2025-02-03 10:34:32 +0600

    developed: top professional section, top marchannts section, top organization section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,19 @@
 import HomeAboutSection from "@/components/home/about/about";
 import CTA_section from "@/components/home/CTA/CTA_section";
 import HeroSection from "@/components/home/hero-section/hero";
-import TopOrganizationCard from "@/components/shared/cards/top-organization-card";
+import TopMarchants from "@/components/home/top-marchants/top-marchants";
+import TopOrganizations from "@/components/home/top-organizations/top-organizatins";
+import TopProfessionals from "@/components/home/top-professional/top-professionals";
 
 export default function Home() {
   return (
     <div className="">
       <HeroSection />
       <HomeAboutSection />
+      <TopProfessionals />
+      <TopMarchants />
+      <TopOrganizations />
       <CTA_section />
-
-      <div className="min-h-screen w-full flex justify-center items-center">
-        <TopOrganizationCard />
-      </div>
     </div>
   );
 }
