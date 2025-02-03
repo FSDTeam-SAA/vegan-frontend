74d4336d84f1a21a84d38affa3c7e4c330411282
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2025-02-03 10:01:09 +0600

    developed top marchant card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import HomeAboutSection from "@/components/home/about/about";
 import CTA_section from "@/components/home/CTA/CTA_section";
 import HeroSection from "@/components/home/hero-section/hero";
+import TopMarchantCard from "@/components/shared/cards/top-marchant-card";
 
 export default function Home() {
   return (
@@ -8,6 +9,10 @@ export default function Home() {
       <HeroSection />
       <HomeAboutSection />
       <CTA_section />
+
+      <div className="min-h-screen w-full flex justify-center items-center">
+        <TopMarchantCard />
+      </div>
     </div>
   );
 }
