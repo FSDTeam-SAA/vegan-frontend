80a74cef27179e6ff509cd3c5519b3c253881f04
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2025-02-03 15:22:53 +0600

    fsd

--- src/components/home/hero-section/hero.tsx
+++ src/components/home/hero-section/hero.tsx
@@ -1,17 +1,17 @@
-import React from "react";
 import MerchantSearch from "./MerchantSearch";
 
+// test
 export default function HeroSection() {
   return (
-    <div className="h-screen  bg-[url('/assets/hero-img.png')] bg-no-repeat bg-cover relative">
-      <div className="w-full h-full flex flex-col justify-center items-center bg-[url('/assets/logo_white.png')] bg-no-repeat bg-opacity text-center bg-center bg-black/40 backdrop:blur-md absolute z-10" />
+    <div className="relative h-screen bg-[url('/assets/hero-img.png')] bg-cover bg-no-repeat">
+      <div className="bg-opacity absolute z-10 flex h-full w-full flex-col items-center justify-center bg-black/40 bg-[url('/assets/logo_white.png')] bg-center bg-no-repeat text-center backdrop:blur-md" />
       {/* hero content  */}
-      <div className="absolute z-20  bg-black/60 backdrop:blur-sm w-full h-full flex flex-col justify-center items-center ">
+      <div className="absolute z-20 flex h-full w-full flex-col items-center justify-center bg-black/60 backdrop:blur-sm">
         <div className="max-w-[792px]">
-          <h1 className="lg:text-[64px] text-[40px] leading-[80px] font-semibold tracking-[-4px] text-white md:mx-8 text-center">
+          <h1 className="text-center text-[40px] font-semibold leading-[80px] tracking-[-4px] text-white md:mx-8 lg:text-[64px]">
             The World&apos;s First All Vegan Marketplace
           </h1>
-          <p className="text-[22px] leading-[33px]  text-white text-center  my-10">
+          <p className="my-10 text-center text-[22px] leading-[33px] text-white">
             Book vegan professionals, shop cruelty-free products, connect with
             charity organizations.
           </p>
