83dadf9b0436b05c1626bfd78a9133809b27e525
Ilhan Khondaker <ilhankhondaker@gmail.com>
2025-02-06 11:46:48 +0600

    add store product card

--- src/app/store/_components/store_products.tsx
+++ src/app/store/_components/store_products.tsx
@@ -1,9 +1,14 @@
 import StoreProductCard from "@/components/shared/cards/store-product-card";
+import { products } from "@/data/StoreProducts";
 
 const StoreProducts = () => {
   return (
-    <div>
-      <StoreProductCard />
+    <div className="mx-auto max-w-7xl">
+      <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+        {products.map((product) => (
+          <StoreProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </div>
   );
 };
