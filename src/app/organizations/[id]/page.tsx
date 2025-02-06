74b8918fea2ca83f4d0965f6c7caad1c14a70b43
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2025-02-06 10:14:05 +0600

    a

---
+++ src/app/organizations/[id]/page.tsx
@@ -0,0 +1,15 @@
+import VeganHeader from "@/components/shared/sections/VeganHeader";
+
+const Page = () => {
+  return (
+    <div>
+      <VeganHeader
+        heading="Green Earth Initiatives"
+        subheading="A leading environmental organization dedicated to protecting and preserving our natural environment through community action, education, and sustainable practices."
+        img="https://res.cloudinary.com/dgnustmny/image/upload/v1738814782/pexels-shvetsa-5029861_1_1_ygagu7.png"
+      />
+    </div>
+  );
+};
+
+export default Page;
