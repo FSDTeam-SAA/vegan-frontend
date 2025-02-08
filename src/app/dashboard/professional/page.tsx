c1711145bf3f1f723c0373130fded71bf400b256
Kongkon-79 <kongkonjowarder@gmail.com>
2025-02-08 17:38:36 +0600

    Leader Board and Referral Tracking section add

--- src/app/dashboard/professional/page.tsx
+++ src/app/dashboard/professional/page.tsx
@@ -1,5 +1,16 @@
+import React from 'react';
+import ReferralTracking from './_components/ReferralTracking';
+import LeaderBoard from './_components/LeaderBoard';
+import TotalEarnings from './_components/TotalEarnings';
+
 const Page = () => {
-  return <div>Professional Overview</div>;
+  return (
+    <div>
+      <TotalEarnings/>
+      <ReferralTracking/>
+      <LeaderBoard/>
+    </div>
+  );
 };
 
 export default Page;
