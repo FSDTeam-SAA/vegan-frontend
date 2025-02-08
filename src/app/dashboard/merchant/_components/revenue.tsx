f206f9bbe8e8a4b1b5d9ef6a850e3e44a0072984
Khalid Hossain Badhon <khalidhossainbadhon@gmail.com>
2025-02-08 18:03:34 +0600

    refactor: dashboard home page content

---
+++ src/app/dashboard/merchant/_components/revenue.tsx
@@ -0,0 +1,79 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
+import { MoreVertical } from "lucide-react";
+import React from "react";
+
+export default function DashboardRevenue() {
+  return (
+    <div>
+      <div className="grid grid-cols-3 gap-6 rounded-xl bg-[#F8F5F2] p-10">
+        <Card className="bg-transparent shadow-none">
+          <CardHeader className="flex flex-row items-center justify-between px-4 pb-2 pt-4">
+            <CardTitle className="pb-4 text-sm font-normal text-[#6B7280]">
+              Total Earnings
+            </CardTitle>
+            <button className="text-gray-400 hover:text-gray-500">
+              <MoreVertical className="h-5 w-5" />
+            </button>
+          </CardHeader>
+          <CardContent className="p-4 pb-3">
+            <div className="mb-[26px] font-inter text-[30px] font-medium leading-[36.31px] text-[#1F2937]">
+              $9,705.21
+            </div>
+            <Button className="font-base w-full bg-[#1D3557] py-[13px] font-inter font-medium leading-[19.36px] hover:bg-[#13243b]">
+              Withdraw Earnings
+            </Button>
+          </CardContent>
+        </Card>
+
+        <Card className="flex flex-col justify-between bg-transparent shadow-none">
+          <CardHeader className="flex flex-row items-center justify-between px-4 pb-2 pt-4">
+            <CardTitle className="pb-4 text-sm font-normal text-[#6B7280]">
+              Product Sales Earnings
+            </CardTitle>
+            <button className="text-gray-400 hover:text-gray-500">
+              <MoreVertical className="h-5 w-5" />
+            </button>
+          </CardHeader>
+          <CardContent className="px-4">
+            <div className="font-inter text-[30px] font-medium leading-[36.31px] text-[#1F2937]">
+              $8,459.32
+            </div>
+          </CardContent>
+          <CardFooter className="p-4">
+            <p className="text-sm font-normal text-[#6B7280]">
+              +15% from last month
+            </p>
+          </CardFooter>
+        </Card>
+
+        <Card className="flex flex-col justify-between bg-transparent shadow-none">
+          <CardHeader className="flex flex-row items-center justify-between px-4 pb-2 pt-4">
+            <CardTitle className="pb-4 text-sm font-normal text-[#6B7280]">
+              Referral Earnings
+            </CardTitle>
+            <button className="text-gray-400 hover:text-gray-500">
+              <MoreVertical className="h-5 w-5" />
+            </button>
+          </CardHeader>
+          <CardContent className="px-4">
+            <div className="font-inter text-[30px] font-medium leading-[36.31px] text-[#1F2937]">
+              $1,245.89
+            </div>
+          </CardContent>
+          <CardFooter className="p-4">
+            <p className="text-sm font-normal text-[#6B7280]">
+              32 active referrals
+            </p>
+          </CardFooter>
+        </Card>
+      </div>
+    </div>
+  );
+}
--- src/data/dashboard.ts
+++ src/data/dashboard.ts
@@ -1,18 +1,19 @@
 "use client";
 import {
+  Box,
   Briefcase,
   CircleHelp,
-  CreditCard,
-  Info,
   LayoutDashboard,
-  ListVideo,
   PhoneCallIcon,
   Truck,
   Users,
   Video,
   Wallet,
 } from "lucide-react";
-import { FaProductHunt, FaSalesforce } from "react-icons/fa";
+import { LuVideo } from "react-icons/lu";
+import { MdStorefront } from "react-icons/md";
+import { BsCreditCard } from "react-icons/bs";
+import { IoIosHelpCircleOutline } from "react-icons/io";
 
 export type NavigationItem = {
   id: string;
@@ -28,56 +29,56 @@ export const merchantTabsList = [
     path: "/dashboard/merchant",
     icon: LayoutDashboard,
     linkText: "Dashboard Overview",
-    roles: ["admin", "user"],
+    roles: ["merchant"],
   },
   {
     id: crypto.randomUUID(),
     path: "/dashboard/merchant/products-management",
-    icon: FaProductHunt,
+    icon: Box,
     linkText: "Product Management",
-    roles: ["admin", "user"],
+    roles: ["merchant"],
   },
   {
     id: crypto.randomUUID(),
     path: "/dashboard/merchant/delivery-management",
     icon: Truck,
     linkText: "Delivery Management",
-    roles: ["admin", "user"],
+    roles: ["merchant"],
   },
   {
     id: crypto.randomUUID(),
     path: "/dashboard/merchant/go-live",
-    icon: ListVideo,
-    linkText: "Go Live & Streaming",
-    roles: ["admin", "user", "delivery man"],
+    icon: LuVideo,
+    linkText: "Go Live",
+    roles: ["merchant"],
   },
   {
     id: crypto.randomUUID(),
     path: "/dashboard/merchant/sales-management",
-    icon: FaSalesforce,
+    icon: MdStorefront,
     linkText: "Sales & Revenue",
-    roles: ["admin", "delivery man"],
+    roles: ["merchant"],
   },
   {
     id: crypto.randomUUID(),
     path: "/dashboard/merchant/payment",
-    icon: CreditCard,
+    icon: BsCreditCard,
     linkText: "Payments & Transactions",
-    roles: ["admin", "user"],
+    roles: ["merchant"],
   },
   {
     id: crypto.randomUUID(),
     path: "/dashboard/merchant/customer-management",
     icon: PhoneCallIcon,
-    linkText: "Customer Relations",
-    roles: ["admin", "user"],
+    linkText: "Customer Management",
+    roles: ["merchant"],
   },
   {
     id: crypto.randomUUID(),
     path: "/dashboard/merchant/support-center",
-    icon: Info,
+    icon: IoIosHelpCircleOutline,
     linkText: "Help & Support Center",
-    roles: ["admin", "delivery man"],
+    roles: ["merchant"],
   },
 ] as NavigationItem[];
 
