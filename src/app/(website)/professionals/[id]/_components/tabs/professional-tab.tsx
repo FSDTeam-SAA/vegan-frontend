a70475bd9565ed7e9e08c2f6b45ca13d40dad187
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2025-02-11 11:30:11 +0600

    a

--- src/app/(website)/professionals/[id]/_components/tabs/professional-tab.tsx
+++ src/app/(website)/professionals/[id]/_components/tabs/professional-tab.tsx
@@ -1,6 +1,5 @@
 "use client";
-import { VeganTab } from "@/components/ui/Vegan-Tab";
-import { LayoutGroup, motion } from "framer-motion";
+import VeganTabs, { VeganTab } from "@/components/ui/Vegan-Tab";
 import { useState } from "react";
 import { AboutTab } from "./about-tab";
 import { ExperienceTab } from "./experience-tab";
@@ -36,53 +35,16 @@ const lists = [
 ] as VeganTab[];
 
 const ProfessionalTab = () => {
-  const [activeTab, setActiveTab] = useState<
-    "about" | "experience" | "services" | "live-stream" | "faqs" | "reviews"
-  >("about");
-  function cn(...classes: string[]): string {
-    return classes.filter(Boolean).join(" ");
-  }
+  const [activeTab, setActiveTab] = useState<string>("about");
 
   return (
     <div className="container mt-[40px] md:mt-[56px]">
       <div className="no-scrollbar overflow-x-auto">
-        <LayoutGroup>
-          <nav className="flex w-full space-x-1 border-b-2 border-[#9CA3AF]">
-            {lists.map((tab) => (
-              <motion.button
-                key={tab.id}
-                onClick={() =>
-                  setActiveTab(
-                    tab.id as
-                      | "about"
-                      | "experience"
-                      | "services"
-                      | "live-stream"
-                      | "faqs"
-                      | "reviews",
-                  )
-                }
-                animate
-                className={cn(
-                  "relative text-nowrap px-4 py-2 text-[18px] font-medium",
-                  "",
-                  activeTab === tab.id
-                    ? "border-b-0 border-primary text-[#1F2937]"
-                    : "text-[#6B7280] hover:text-gray-700",
-                )}
-              >
-                {activeTab === tab.id && (
-                  <motion.div
-                    className="absolute bottom-[-4px] left-0 h-[4px] w-full"
-                    layoutId="underline"
-                    style={{ backgroundColor: "#1F2937" }}
-                  />
-                )}
-                {tab.label}
-              </motion.button>
-            ))}
-          </nav>
-        </LayoutGroup>
+        <VeganTabs
+          tabs={lists}
+          defaultActiveTab={activeTab}
+          onTabChange={(tab) => setActiveTab(tab)}
+        />
       </div>
       <div className="mt-[40px]">
         {activeTab === "about" && <AboutTab />}
