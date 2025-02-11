b93f8ba5d6660aaf1fad6d012a55b38dbd2c1370
Kongkon-79 <kongkonjowarder@gmail.com>
2025-02-11 10:15:06 +0600

    chart responsive

--- src/app/dashboard/professional/client-management/page.tsx
+++ src/app/dashboard/professional/client-management/page.tsx
@@ -4,7 +4,7 @@ import ClientManagementContainer from './_components/ClientManagementContainer';
 
 const Page = () => {
     return (
-        <div className=' pt-[32px] md:pt-[40px] pb-[24px] md:pb-[40px] lg:pb-[56px]'>
+        <div className='px-10 pt-[32px] md:pt-[40px] pb-[24px] md:pb-[40px] lg:pb-[56px]'>
             <ClientManagementHeader/>
             <ClientManagementContainer/>
         </div>
