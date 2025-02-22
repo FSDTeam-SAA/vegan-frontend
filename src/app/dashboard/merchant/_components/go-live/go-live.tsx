c0b2330df3f4237bf5f2619bb274cd95a7bbaca7
Monir Hossain <monirhrabby.personal@gmail.com>
2025-02-22 14:46:48 +0600

    developed: professional service create feature

--- src/app/dashboard/merchant/_components/go-live/go-live.tsx
+++ src/app/dashboard/merchant/_components/go-live/go-live.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import EventsMangement from "./events";
+import dynamic from "next/dynamic";
 import MerchantTutiorials from "../../go-live/MerchantTutorials";
+const EventsMangement = dynamic(() => import("./events"), { ssr: false });
 
 export default function GoLive() {
   return (
