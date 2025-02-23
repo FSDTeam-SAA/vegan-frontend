cab5157475946242c17397f5847628a7f5b562f4
Monir Hossain <monirhrabby.personal@gmail.com>
2025-02-23 15:54:47 +0600

    a

--- src/app/dashboard/organization/_components/event/view-attendees.tsx
+++ src/app/dashboard/organization/_components/event/view-attendees.tsx
@@ -1,11 +1,37 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import VeganPagination from "@/components/ui/vegan-pagination";
 import { OrganizationEvent } from "@/types/organization";
+import { useState } from "react";
 
 interface Props {
   data: OrganizationEvent;
 }
 
 const ViewAttendees = ({}: Props) => {
-  return <div>ViewAttendees</div>;
+  const [currentPage, setCurrentPage] = useState(1);
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="link">View Attendees</Button>
+      </DialogTrigger>
+      <DialogContent className="p-0 sm:max-w-[425px]">
+        fsdfds
+        <DialogFooter>
+          <VeganPagination
+            currentPage={currentPage}
+            onPageChange={(page) => setCurrentPage(page)}
+            totalPages={5}
+          />
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default ViewAttendees;
