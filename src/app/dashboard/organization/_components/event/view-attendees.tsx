526f30b2fed4cf279c30047f9845b996393165c0
Monir Hossain <monirhrabby.personal@gmail.com>
2025-02-23 15:48:21 +0600

    developed: filter with event category

---
+++ src/app/dashboard/organization/_components/event/view-attendees.tsx
@@ -0,0 +1,11 @@
+import { OrganizationEvent } from "@/types/organization";
+
+interface Props {
+  data: OrganizationEvent;
+}
+
+const ViewAttendees = ({}: Props) => {
+  return <div>ViewAttendees</div>;
+};
+
+export default ViewAttendees;
