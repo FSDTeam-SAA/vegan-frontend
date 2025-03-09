8200c25c32fe94124e5e025961f19c59de5a1b77
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2025-03-09 19:45:22 +0600

    developed: reffer

--- src/app/dashboard/merchant/_components/ReferralTracking.tsx
+++ src/app/dashboard/merchant/_components/ReferralTracking.tsx
@@ -1,7 +1,9 @@
 "use client";
+import ErrorContainer from "@/components/shared/sections/error-container";
 import { Button } from "@/components/ui/button";
+import SkeletonWrapper from "@/components/ui/skeleton-wrapper";
 import { ReferResponse } from "@/types/refferel";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { EllipsisVertical, Loader2, Share2 } from "lucide-react";
 import dynamic from "next/dynamic";
 import { useState } from "react";
@@ -19,6 +21,14 @@ interface Props {
 const ReferralTracking = ({ userId }: Props) => {
   const [isOpen, setOpen] = useState(false);
   const [url, setUrl] = useState("");
+
+  const { isLoading, data, isError, error } = useQuery<ReferResponse>({
+    queryKey: ["my-refer-stats"],
+    queryFn: () =>
+      fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/reffer/${userId}`).then(
+        (res) => res.json(),
+      ),
+  });
 
   const { isPending, mutate: createSlug } = useMutation<ReferResponse>({
     mutationKey: ["refer-slug-generate"],
@@ -49,14 +59,12 @@ const ReferralTracking = ({ userId }: Props) => {
     },
   });
 
-  return (
-    <div className="">
-      <div className="rounded-[16px] bg-[#F8F5F2] p-[24px] md:p-[32px] lg:p-[40px]">
-        <h4 className="pb-[32px] text-lg font-medium leading-[21px] text-[#1F2937] md:pb-[40px] md:text-xl md:leading-[24px]">
-          Referral Tracking
-        </h4>
+  let content;
 
-        <div className="grid grid-cols-1 gap-[16px] pb-[56px] md:grid-cols-3">
+  if (isLoading) {
+    content = (
+      <div className="grid grid-cols-1 gap-[16px] pb-[56px] md:grid-cols-3">
+        <SkeletonWrapper isLoading={isLoading}>
           <div className="rounded-[10px] border border-[#E8DFD6] py-4 pl-4 pr-[6px] md:col-span-1">
             <p className="flex items-center justify-between text-sm font-normal leading-[16px] text-[#6B7280]">
               Total Referrals <EllipsisVertical className="h-[24px] w-[24px]" />
@@ -65,6 +73,8 @@ const ReferralTracking = ({ userId }: Props) => {
               68
             </p>
           </div>
+        </SkeletonWrapper>
+        <SkeletonWrapper isLoading={isLoading}>
           <div className="rounded-[10px] border border-[#E8DFD6] py-4 pl-4 pr-[6px] md:col-span-1">
             <p className="flex items-center justify-between text-sm font-normal leading-[16px] text-[#6B7280]">
               Amount Deducted For Charity{" "}
@@ -74,6 +84,8 @@ const ReferralTracking = ({ userId }: Props) => {
               $100.00
             </p>
           </div>
+        </SkeletonWrapper>
+        <SkeletonWrapper isLoading={isLoading}>
           <div className="rounded-[10px] border border-[#E8DFD6] py-4 pl-4 pr-[6px] md:col-span-1">
             <p className="flex items-center justify-between text-sm font-normal leading-[16px] text-[#6B7280]">
               Commission Paid <EllipsisVertical className="h-[24px] w-[24px]" />
@@ -82,7 +94,55 @@ const ReferralTracking = ({ userId }: Props) => {
               $524.00
             </p>
           </div>
+        </SkeletonWrapper>
+      </div>
+    );
+  } else if (isError) {
+    content = (
+      <ErrorContainer
+        message={error?.message ?? "Failed to load reffer data"}
+      />
+    );
+  } else if (data) {
+    content = (
+      <div className="grid grid-cols-1 gap-[16px] pb-[56px] md:grid-cols-3">
+        <div className="rounded-[10px] border border-[#E8DFD6] py-4 pl-4 pr-[6px] md:col-span-1">
+          <p className="flex items-center justify-between text-sm font-normal leading-[16px] text-[#6B7280]">
+            Total Referrals <EllipsisVertical className="h-[24px] w-[24px]" />
+          </p>
+          <p className="pt-[16px] text-2xl font-medium leading-[29px] text-[#1F2937] md:text-3xl md:leading-[36px]">
+            68
+          </p>
         </div>
+        <div className="rounded-[10px] border border-[#E8DFD6] py-4 pl-4 pr-[6px] md:col-span-1">
+          <p className="flex items-center justify-between text-sm font-normal leading-[16px] text-[#6B7280]">
+            Amount Deducted For Charity{" "}
+            <EllipsisVertical className="h-[24px] w-[24px]" />
+          </p>
+          <p className="pt-[16px] text-2xl font-medium leading-[29px] text-[#1F2937] md:text-3xl md:leading-[36px]">
+            $100.00
+          </p>
+        </div>
+        <div className="rounded-[10px] border border-[#E8DFD6] py-4 pl-4 pr-[6px] md:col-span-1">
+          <p className="flex items-center justify-between text-sm font-normal leading-[16px] text-[#6B7280]">
+            Commission Paid <EllipsisVertical className="h-[24px] w-[24px]" />
+          </p>
+          <p className="pt-[16px] text-2xl font-medium leading-[29px] text-[#1F2937] md:text-3xl md:leading-[36px]">
+            $524.00
+          </p>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="">
+      <div className="rounded-[16px] bg-[#F8F5F2] p-[24px] md:p-[32px] lg:p-[40px]">
+        <h4 className="pb-[32px] text-lg font-medium leading-[21px] text-[#1F2937] md:pb-[40px] md:text-xl md:leading-[24px]">
+          Referral Tracking
+        </h4>
+
+        {content}
 
         <div>
           <h6 className="text-lg font-semibold leading-[21px] text-[#1F2937]">
