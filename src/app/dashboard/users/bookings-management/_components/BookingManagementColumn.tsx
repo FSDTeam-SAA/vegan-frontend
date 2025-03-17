9559a7b58b0f4e749c86943ba2cecc85afa8d312
Monir Hossain <monirhrabby.personal@gmail.com>
2025-03-17 10:25:44 +0600

    developed: booking management

--- src/app/dashboard/users/bookings-management/_components/BookingManagementColumn.tsx
+++ src/app/dashboard/users/bookings-management/_components/BookingManagementColumn.tsx
@@ -1,7 +1,8 @@
 "use client";
-import React, { useState } from "react";
+import ClientManagementServiceBookingAction from "@/app/dashboard/professional/client-management/_components/ClientManagementServiceBookingAction";
 import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
+import { toWellFormed } from "@/lib/helper";
+import { ServiceBooking } from "@/types/professional";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -10,16 +11,13 @@ import {
 } from "@radix-ui/react-dropdown-menu";
 import { ColumnDef } from "@tanstack/react-table";
 import { EllipsisVertical } from "lucide-react";
-import { BookingManagementDataTypes } from "./BookingManagementData";
-import Image from "next/image";
+import moment from "moment";
+import { useState } from "react";
 import BookingDetailsForm from "./BookingDetailsForm";
-
-
-
 
 // ActionsDropdown component  start
 
-const ActionsDropdown = ({id} : {id : number}) => {
+const ActionsDropdown = ({ id }: { id: number }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleFormData = () => {
@@ -43,11 +41,11 @@ const ActionsDropdown = ({id} : {id : number}) => {
           >
             <DropdownMenuItem
               onClick={handleFormData}
-              className="p-[8px] text-xs font-normal leading-[14px] text-[#1F2937] hover:bg-[#E6EEF6] focus:outline-none focus:ring-0 focus-visible:ring-0 focus-visible:ring-offset-0 cursor-pointer"
+              className="cursor-pointer p-[8px] text-xs font-normal leading-[14px] text-[#1F2937] hover:bg-[#E6EEF6] focus:outline-none focus:ring-0 focus-visible:ring-0 focus-visible:ring-offset-0"
             >
               Reschedule Booking
             </DropdownMenuItem>
-            <DropdownMenuItem className="rounded-b-[8px] p-[8px] text-xs font-normal leading-[14px] text-[#EF4444] hover:bg-[#E6EEF6] focus:outline-none focus:ring-0 focus-visible:ring-0 focus-visible:ring-offset-0 cursor-pointer">
+            <DropdownMenuItem className="cursor-pointer rounded-b-[8px] p-[8px] text-xs font-normal leading-[14px] text-[#EF4444] hover:bg-[#E6EEF6] focus:outline-none focus:ring-0 focus-visible:ring-0 focus-visible:ring-offset-0">
               Cancel Booking
             </DropdownMenuItem>
           </DropdownMenuContent>
@@ -61,7 +59,7 @@ const ActionsDropdown = ({id} : {id : number}) => {
           <section className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-30 backdrop-blur-sm">
             {/* Modal content */}
             <div className="relative z-10">
-              <BookingDetailsForm setIsOpen={setIsOpen} bookingId ={id}/>
+              <BookingDetailsForm setIsOpen={setIsOpen} bookingId={id} />
             </div>
           </section>
         )}
@@ -74,135 +72,115 @@ export default ActionsDropdown;
 
 // ActionsDropdown component  end
 
-export const BookingManagementColumn: ColumnDef<BookingManagementDataTypes>[] =
-  [
-    {
-      id: "select",
-      header: ({ table }) => (
-        <div className="flex items-center justify-center">
-          <Checkbox
-            checked={
-              table.getIsAllPageRowsSelected() ||
-              (table.getIsSomePageRowsSelected() && "indeterminate")
-            }
-            onCheckedChange={(value) =>
-              table.toggleAllPageRowsSelected(!!value)
-            }
-            aria-label="Select all"
-          />
+export const BookingManagementColumn: ColumnDef<ServiceBooking>[] = [
+  {
+    header: "Booking ID",
+    cell: ({ row }) => {
+      return (
+        <div className="flex justify-start gap-[2px]">
+          <span className="text-sm font-normal leading-[16px] text-[#1F2937]">
+            {row.original.bookingID}
+          </span>
         </div>
-      ),
-      cell: ({ row }) => (
-        <Checkbox
-          checked={row.getIsSelected()}
-          onCheckedChange={(value) => row.toggleSelected(!!value)}
-          aria-label="Select row"
-        />
-      ),
-      enableSorting: false,
-      enableHiding: false,
+      );
     },
-    {
-      header: "Booking ID",
-      cell: ({ row }) => {
-        return (
-          <div className="flex justify-start gap-[2px]">
-            <span className="text-sm font-normal leading-[16px] text-[#1F2937]">
-              {row.original.bookingId}
-            </span>
-          </div>
-        );
-      },
-    },
-    {
-      header: "Professional Name",
-      cell: ({ row }) => {
-        console.log(row);
-        return (
-          <div className="flex items-center gap-4">
-            <div>
-              <Image
+  },
+  {
+    header: "Professional Name",
+    cell: ({ row }) => {
+      console.log(row);
+      return (
+        <div className="flex items-center gap-4">
+          <div>
+            {/* <Image
                 src={row.original.img}
                 height={28}
                 width={28}
                 alt="img"
                 className="rounded-full"
-              />
-            </div>
-            <div className="text-left">
-              <h4 className="text-sm font-medium leading-[16px] text-[#1F2937]">
-                {row.original.name}
-              </h4>
-              <h4 className="pt-[8px] text-xs font-normal leading-[14px] text-[#6B7280]">
-                {row?.original?.email}
-              </h4>
-            </div>
+              /> */}
           </div>
-        );
-      },
-    },
-    {
-      header: "Service Title",
-      cell: ({ row }) => {
-        return (
-          <div className="flex justify-start gap-[2px]">
-            <span className="text-sm font-normal leading-[16px] text-[#1F2937]">
-              {row.original.serviceTitle}
-            </span>
+          <div className="text-left">
+            <h4 className="text-sm font-medium leading-[16px] text-[#1F2937]">
+              {row.original.userID.fullName}
+            </h4>
+            <h4 className="pt-[8px] text-xs font-normal leading-[14px] text-[#6B7280]">
+              {row?.original?.userID.email}
+            </h4>
           </div>
-        );
-      },
+        </div>
+      );
     },
-    {
-      header: "Amount",
-      cell: ({ row }) => {
-        return (
-          <div className="flex justify-start gap-[2px]">
-            <span className="text-sm font-normal leading-[16px] text-[#1F2937]">
-              {row.original.amout}
-            </span>
-          </div>
-        );
-      },
+  },
+  {
+    header: "Service Title",
+    cell: ({ row }) => {
+      return (
+        <div className="flex justify-start gap-[2px]">
+          <span className="text-sm font-normal leading-[16px] text-[#1F2937]">
+            {row.original.professionalServicesId?.serviceName ??
+              "NO SERVICE NAME FOUND"}
+          </span>
+        </div>
+      );
     },
-    {
-      header: "Date",
-      cell: ({ row }) => {
-        return (
-          <div className="flex justify-start gap-[2px]">
-            <span className="text-sm font-normal leading-[16px] text-[#1F2937]">
-              {row.original.date}
-            </span>
-          </div>
-        );
-      },
+  },
+  {
+    header: "Amount",
+    cell: ({ row }) => {
+      return (
+        <div className="flex justify-start gap-[2px]">
+          <span className="text-sm font-normal leading-[16px] text-[#1F2937]">
+            {row.original.amount}
+          </span>
+        </div>
+      );
     },
-    {
-      header: "Time",
-      cell: ({ row }) => {
-        return (
-          <div className="flex justify-start gap-[2px]">
-            <span className="text-sm font-normal leading-[16px] text-[#1F2937]">
-              {row.original.time}
-            </span>
-          </div>
-        );
-      },
+  },
+  {
+    header: "Date",
+    cell: ({ row }) => {
+      return (
+        <div className="flex justify-start gap-[2px]">
+          <span className="text-sm font-normal leading-[16px] text-[#1F2937]">
+            {moment(row.original.createdAt).format("MMM DD, YYYY")}
+          </span>
+        </div>
+      );
     },
-    {
-      header: "Status",
-      cell: ({ row }) => {
-        return (
-          <div className="flex justify-start gap-[2px]">
-            <span className="rounded-[20px] bg-[#EFF6FF] px-2 py-1 text-sm font-medium leading-[14px] text-[#2563EB]">
-              {row.original.status}
-            </span>
-          </div>
-        );
-      },
+  },
+  {
+    header: "Time",
+    cell: ({ row }) => {
+      return (
+        <div className="flex justify-start gap-[2px]">
+          <span className="text-sm font-normal leading-[16px] text-[#1F2937]">
+            {row.original.serviceBookingTime}
+          </span>
+        </div>
+      );
     },
-    {
-      header: "Actions",
-      cell: ({row}) => <ActionsDropdown id={row?.original?.id} />
+  },
+  {
+    header: "Status",
+    cell: ({ row }) => {
+      const status = row.original.status;
+
+      return (
+        <div className="flex justify-start gap-[2px]">
+          <span
+            className={`rounded-[20px] px-[8px] py-[4px] text-sm font-medium leading-[16px] ${status === "confirmed" ? "bg-[#F0FDF4] text-[#16A34A]" : "bg-[#F8F5F2] text-[#CA8A04]"}`}
+          >
+            {toWellFormed(status)}
+          </span>
+        </div>
+      );
     },
-  ];
+  },
+  {
+    header: "Actions",
+    cell: ({ row }) => (
+      <ClientManagementServiceBookingAction data={row.original} />
+    ),
+  },
+];
--- src/app/dashboard/users/bookings-management/_components/UpcomingBooking.tsx
+++ src/app/dashboard/users/bookings-management/_components/UpcomingBooking.tsx
@@ -1,15 +1,12 @@
-import React from "react";
-import BookingManagementContainer from "./BookingManagementContainer";
-import SearchBar from "./SearchBar";
-
 const UpcomingBooking = () => {
   return (
-    <div>
-      <div className="pt-10">
-        <SearchBar />
-      </div>
-      <BookingManagementContainer />
-    </div>
+    // <div>
+    //   <div className="pt-10">
+    //     <SearchBar />
+    //   </div>
+    //   <BookingManagementContainer />
+    // </div>
+    <></>
   );
 };
 
