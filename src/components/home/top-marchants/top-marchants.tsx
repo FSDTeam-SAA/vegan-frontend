c5a2f2447443b76f04b1b70023bb7e9b1dee7912
Monir Hossain <monirhrabby.personal@gmail.com>
2025-03-20 12:16:02 +0600

    developed: top merchants feature

--- src/components/home/top-marchants/top-marchants.tsx
+++ src/components/home/top-marchants/top-marchants.tsx
@@ -1,8 +1,79 @@
+"use client";
 import TopMarchantCard from "@/components/shared/cards/top-marchant-card";
+import EmptyContainer from "@/components/shared/sections/empty-container";
+import ErrorContainer from "@/components/shared/sections/error-container";
+import SkeletonWrapper from "@/components/ui/skeleton-wrapper";
+import { useQuery } from "@tanstack/react-query";
 import { MoveRight } from "lucide-react";
 import Link from "next/link";
 
+export type MerchantData = {
+  totalSales: number;
+  totalOrders: number;
+  merchantID?: string;
+  businessName?: string;
+  profilePhoto?: string;
+  about?: string;
+  shortDescriptionOfStore?: string;
+  address?: string;
+};
+
+type Pagination = {
+  currentPage: number;
+  totalPages: number;
+  totalMerchants: number;
+  limit: number;
+};
+
+type ResponseData = {
+  success: boolean;
+  data: MerchantData[];
+  pagination: Pagination;
+};
+
 const TopMarchants = () => {
+  const { isLoading, data, isError, error } = useQuery<ResponseData>({
+    queryKey: ["top-merchants"],
+    queryFn: () =>
+      fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/top-merchants?page=1&limit=10`,
+      ).then((res) => res.json()),
+  });
+
+  let content;
+
+  if (isLoading) {
+    content = (
+      <div className="mt-[54px] grid grid-cols-1 gap-[24px] md:mt-[80px] md:grid-cols-2 lg:grid-cols-3">
+        {[1, 2, 3, 4, 5, 6].map((n) => (
+          <SkeletonWrapper isLoading={isLoading} key={n}>
+            <TopMarchantCard />
+          </SkeletonWrapper>
+        ))}
+      </div>
+    );
+  } else if (isError) {
+    content = (
+      <div className="mt-5">
+        <ErrorContainer
+          message={error?.message ?? "Failed to load top professional"}
+        />
+      </div>
+    );
+  } else if (data && data.data.length === 0) {
+    content = (
+      <EmptyContainer message="No Top professional found on vegan collective" />
+    );
+  } else if (data) {
+    content = (
+      <div className="mt-[54px] grid grid-cols-1 gap-[24px] md:mt-[80px] md:grid-cols-2 lg:grid-cols-3">
+        {data.data.map((item) => (
+          <TopMarchantCard key={item.merchantID} data={item} />
+        ))}
+      </div>
+    );
+  }
+
   return (
     <div className="container py-[48px] md:py-[80px]">
       <div className="flex w-full items-center justify-between">
@@ -18,11 +89,7 @@ const TopMarchants = () => {
         </Link>
       </div>
 
-      <div className="mt-[54px] grid grid-cols-1 gap-[24px] md:mt-[80px] md:grid-cols-2 lg:grid-cols-3">
-        {[1, 2, 3, 4, 5, 6].map((n) => (
-          <TopMarchantCard key={n} />
-        ))}
-      </div>
+      {content}
     </div>
   );
 };
--- src/components/shared/cards/top-marchant-card.tsx
+++ src/components/shared/cards/top-marchant-card.tsx
@@ -1,8 +1,15 @@
+import { MerchantData } from "@/components/home/top-marchants/top-marchants";
+import { truncateText } from "@/lib/helper";
 import { MapPin } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
-const TopMarchantCard = () => {
+interface Props {
+  data?: MerchantData;
+}
+
+const TopMarchantCard = ({ data }: Props) => {
+  const about = truncateText(data?.shortDescriptionOfStore ?? "", 91);
   return (
     <Link
       href="/merchants/123"
@@ -10,7 +17,10 @@ const TopMarchantCard = () => {
     >
       <div className="relative h-[232px] w-[159px] rounded-[16px]">
         <Image
-          src="https://i.postimg.cc/DzBm1ynN/Frame-1618873695.png"
+          src={
+            data?.profilePhoto ??
+            "https://i.postimg.cc/DzBm1ynN/Frame-1618873695.png"
+          }
           alt="profile"
           className="rounded-[16px] object-cover"
           fill
@@ -18,14 +28,13 @@ const TopMarchantCard = () => {
       </div>
       <div className="space-y-[16px]">
         <h3 className="font-lexend text-[18px] leading-[22.5px] tracking-[-1px] text-[#1D3557]">
-          Vegan Fitness Trainer
+          {data?.businessName}
         </h3>
         <p className="max-w-[170px] font-inter text-[14px] leading-[21px] text-[#4B5563]">
-          I am here our certified trainers are here to guide and support you on
-          your fitness journey.
+          {about}
         </p>
         <p className="flex items-center gap-x-1 text-[14px] font-normal text-[#4B5563]">
-          <MapPin /> Shefiled, USA
+          <MapPin /> {data?.address}
         </p>
       </div>
     </Link>
