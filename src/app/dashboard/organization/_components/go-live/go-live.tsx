75e7cf4274a01fd4e8747c9152251af75c00c97c
Monir Hossain <monirhrabby.personal@gmail.com>
2025-04-09 14:24:11 +0600

    developed: organizatin meeting create feature

--- src/app/dashboard/organization/_components/go-live/go-live.tsx
+++ src/app/dashboard/organization/_components/go-live/go-live.tsx
@@ -1,11 +1,15 @@
 import VideoTutorials from "../shared/VideoTutorials";
 import EventsMangement from "./events";
 import { Header } from "./header";
+interface Props {
+  email: string;
+  userId: string;
+}
 
-export default function GoLive() {
+export default function GoLive({ email, userId }: Props) {
   return (
     <div className="space-y-10">
-      <Header />
+      <Header email={email} userId={userId} />
       <EventsMangement />
       <VideoTutorials />
     </div>
