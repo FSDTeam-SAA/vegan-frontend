ed4e81d477bb9dd3315e4db10dd8e31f12282e7f
Monir Hossain <monirhrabby.personal@gmail.com>
2025-04-21 14:05:19 +0600

    remov

--- src/provider/plausible-analytics-provider.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { ReactNode } from "react";
-
-const PlaxibleAnalyticsProvider = ({ children }: { children: ReactNode }) => {
-  return <div>PlaxibleAnalyticsProvider</div>;
-};
-
-export default PlaxibleAnalyticsProvider;
